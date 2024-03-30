8f9187934a2cb98670a2d26720005513cd53e1ed
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-03-30 23:41:52 +0500

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Томирис Рахымжан on 30/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
